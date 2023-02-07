afb7fd992f868ee11f45f006844b0512b7dd623a
Robin Müller <robin.mueller@outlook.de>
2023-02-07 23:18:11 +0100

    Initial Commit

---
+++ Ro.CI.Demo/Main.cs
@@ -0,0 +1,11 @@
+﻿namespace Ro.CI.Demo;
+
+public static class Main
+{
+    public const string Demo = "Hello World";
+
+    public static void Run()
+    {
+        Console.WriteLine(Demo);
+    }
+}
